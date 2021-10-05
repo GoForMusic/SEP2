615abb2118566240b8a1c4880fef7e328e79a83e
Adrian <militaruadriancristian@gmail.com>
2021-10-05 22:17:15 +0200

    SEP2-7

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args)
     {
-        System.out.print("Welcome from Github");
+        System.out.print("Welcome from Github update JIRA");
     }
 }
