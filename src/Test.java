d4b3b4a9d372061bc43c959a4d440564226d8845
Adrian <militaruadriancristian@gmail.com>
2021-10-06 12:51:04 +0200

    SEP2-17

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args)
     {
-        System.out.print("Welcome from Github update JIRA");
+        System.out.print("Update Emil branch");
     }
 }
