a3e9666af658e5c2bd301e2873cd9efbe0d8fbb7
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-12 19:44:34 +0100

    changes here and there

---
+++ Semester Project/src/client/networking/GetServer.java
@@ -0,0 +1,26 @@
+package client.networking;
+
+import shared.networking.serverInterfaces.Server;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class GetServer {
+    public static Server getServerFromRmi(){
+        Registry registry=null;
+        Server server = null;
+        try {
+            registry = LocateRegistry.getRegistry(1099);
+            server = (Server) registry.lookup("Server");
+
+        } catch (RemoteException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Cannot connect to server");
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+        return server;
+    }
+}
--- Semester Project/src/client/networking/login/LoginClientImpl.java
+++ Semester Project/src/client/networking/login/LoginClientImpl.java
@@ -1,26 +1,17 @@
 package client.networking.login;
 
+import client.networking.GetServer;
 import shared.networking.serverInterfaces.Server;
 import shared.utils.User.User;
 
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class LoginClientImpl implements LoginClient{
 
     private Server server;
 
     public LoginClientImpl(){
-        try {
-            Registry registry = LocateRegistry.getRegistry(1099);
-            server =(Server) registry.lookup("Server");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
+      server = GetServer.getServerFromRmi();
     }
 
 
