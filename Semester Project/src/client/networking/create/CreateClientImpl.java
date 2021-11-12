9b5e30d71859db71f114319ae594b47b6d3f9068
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-12 19:44:34 +0100

    changes here and there

--- Semester Project/src/client/networking/create/CreateClientImpl.java
+++ Semester Project/src/client/networking/create/CreateClientImpl.java
@@ -1,27 +1,16 @@
 package client.networking.create;
 
+import client.networking.GetServer;
 import shared.networking.serverInterfaces.Server;
 import shared.utils.User.User;
 
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class CreateClientImpl implements CreateClient {
     private Server server;
 
     public CreateClientImpl() {
-        Registry registry = null;
-        try {
-            registry = LocateRegistry.getRegistry(1099);
-            server = (Server) registry.lookup("Server");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
-
+           server = GetServer.getServerFromRmi();
     }
     @Override
     public String addUser(User user) {
