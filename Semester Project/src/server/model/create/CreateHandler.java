95693a09ded5d518942df0f4c1847a1182a0d66f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-12 22:22:32 +0100

    minor changes

--- Semester Project/src/server/model/create/CreateHandler.java
+++ Semester Project/src/server/model/create/CreateHandler.java
@@ -3,7 +3,7 @@ package server.model.create;
 import shared.utils.User.User;
 
 public interface CreateHandler {
-    boolean doesUsernameExists(String username);
+
 
     String addUser(User user);
 }
--- Semester Project/src/shared/networking/serverInterfaces/CreateAccountServer.java
+++ Semester Project/src/shared/networking/serverInterfaces/CreateAccountServer.java
@@ -6,6 +6,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface CreateAccountServer extends Remote {
-    boolean doesUsernameExists(String username) throws RemoteException;
     String addUser(User user) throws RemoteException;
 }
