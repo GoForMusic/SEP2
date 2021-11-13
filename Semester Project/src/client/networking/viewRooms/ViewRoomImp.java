978c8a7638d86343824521cb1b4633608b27aa61
VassilevEmil <84077074+VassilevEmil@users.noreply.github.com>
2021-11-13 22:23:22 +0100

    SEP2-42 done

--- Semester Project/src/client/networking/viewRooms/ViewRoomImp.java
+++ Semester Project/src/client/networking/viewRooms/ViewRoomImp.java
@@ -7,13 +7,27 @@ import shared.utils.room.RoomType;
 import java.rmi.RemoteException;
 import java.time.LocalDate;
 
+/** @author Emil
+ * creating a class for the rooms view
+ *
+ */
 public class ViewRoomImp implements ViewRoomClient {
     private Server server;
+
+    /**
+     * Constructor initializing the server
+     */
 
     public ViewRoomImp() {
         server = GetServer.getServerFromRmi();
     }
 
+    /**
+     *
+     * @param dateFrom the date to search from
+     * @param dateTo the date to search upto
+     * @param category the category of the room to search
+     */
     @Override
     public void searchRooms(LocalDate dateFrom, LocalDate dateTo, RoomType roomType) {
         try {
