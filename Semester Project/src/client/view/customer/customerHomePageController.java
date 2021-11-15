639d9a8097eec3f6cec9d4c0b6222a1547d238c4
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-15 15:22:42 +0100

    for himal

--- Semester Project/src/client/view/customer/customerHomePageController.java
+++ Semester Project/src/client/view/customer/customerHomePageController.java
@@ -4,36 +4,35 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.view.ViewController;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
-import java.io.IOException;
+public class customerHomePageController implements ViewController {
 
-public class customerHomePageController implements ViewController
-{
+    @FXML
+    private BorderPane customerBorderPane;
+    private ViewHandler viewHandler;
 
-  @FXML private BorderPane customerBorderPane;
-
-  @FXML private void clickedCustomerHome(MouseEvent mouseEvent)
-  {
-
-  }
-
-  @FXML private void clickedCustomerViewRoom(MouseEvent mouseEvent)
-  {
-
-  }
-
-  @FXML private void clickedCustomerMyAccount(MouseEvent mouseEvent)
-  {
-
-  }
+    @Override
+    public void init(ViewHandler vh, ViewModelFactory vmf) {
+        this.viewHandler = vh;
+    }
 
 
-  @Override public void init(ViewHandler vh, ViewModelFactory vmf)
-  {
+    @FXML
+    private void clickedCustomerHome(MouseEvent mouseEvent) {
 
-  }
+    }
+
+    @FXML
+    private void clickedCustomerViewRoom(MouseEvent mouseEvent) {
+        customerBorderPane.setCenter(viewHandler.getViewRoomPane());
+    }
+
+    @FXML
+    private void clickedCustomerMyAccount(MouseEvent mouseEvent) {
+
+    }
+
+
 }
