48299571c96758c8b0750b2b81ee7b89eb383f43
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-30 23:56:13 +0100

    now customer cannot select past dates..

--- Semester Project/src/client/view/customer/doubleBedroom/CustomerDoubleBedRoomController.java
+++ Semester Project/src/client/view/customer/doubleBedroom/CustomerDoubleBedRoomController.java
@@ -4,10 +4,13 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.view.ViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+
+import java.time.LocalDate;
 
 public class CustomerDoubleBedRoomController implements ViewController {
 
@@ -32,12 +35,23 @@ public class CustomerDoubleBedRoomController implements ViewController {
         customerDoubleDateTo.valueProperty().bindBidirectional(viewModel.getDateTo());
         price.textProperty().bind(viewModel.getPricePerNight());
         description.textProperty().bind(viewModel.getDescription());
+        disablePast(customerDoubleDateFrom);
+        disablePast(customerDoubleDateTo);
     }
     @FXML
     private void searchRooms(){
         anchorpane.getChildren().clear();
         anchorpane.getChildren().setAll(viewHandler.getRoomList());
         viewModel.searchRooms();
+    }
+    private void disablePast(DatePicker dp){
+        dp.setDayCellFactory(picker -> new DateCell() {
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                LocalDate today = LocalDate.now();
+                setDisable(empty || date.compareTo(today) < 0 );
+            }
+        });
     }
 
 }
