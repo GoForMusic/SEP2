67bc9e7c1da6b32b4252600d5d688e1fe2a08c15
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-11-30 23:56:13 +0100

    now customer cannot select past dates..

--- Semester Project/src/client/view/customer/singleRoom/SingleBedRoomController.java
+++ Semester Project/src/client/view/customer/singleRoom/SingleBedRoomController.java
@@ -4,10 +4,13 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.view.ViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+
+import java.time.LocalDate;
 
 
 /**
@@ -39,12 +42,23 @@ public class SingleBedRoomController implements ViewController {
         customerSingleDateto.valueProperty().bindBidirectional(viewModel.getDateTo());
         price.textProperty().bind(viewModel.getPrice());
         description.textProperty().bind(viewModel.getDescription());
+        disablePast(customerSingleDateFrom);
+        disablePast(customerSingleDateto);
     }
     @FXML
     private void searchRoom(){
         anchorpane.getChildren().clear();
         anchorpane.getChildren().setAll(viewHandler.getRoomList());
         viewModel.searchRooms();
+    }
+    private void disablePast(DatePicker dp){
+        dp.setDayCellFactory(picker -> new DateCell() {
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                LocalDate today = LocalDate.now();
+                setDisable(empty || date.compareTo(today) < 0 );
+            }
+        });
     }
 
 
