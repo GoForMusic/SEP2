b8f9a07ca911334a949fbe605d76d02ebfe3e4b2
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-12-01 17:39:45 +0100

    trying to display the customer booking..

--- Semester Project/src/client/view/receptionist/searchReservation/SearchCustomerReservationToEditController.java
+++ Semester Project/src/client/view/receptionist/searchReservation/SearchCustomerReservationToEditController.java
@@ -4,6 +4,7 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.view.ViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -15,6 +16,8 @@ import java.time.LocalDate;
 public class SearchCustomerReservationToEditController implements ViewController {
     private ViewHandler viewHandler;
 
+    @FXML
+    private Label error;
     @FXML
     private TextField username;
     @FXML
@@ -32,6 +35,8 @@ public class SearchCustomerReservationToEditController implements ViewController
     public void init(ViewHandler vh, ViewModelFactory vmf) {
         this.viewHandler = vh;
         viewModel = vmf.getSearchReservationViewModel();
+        username.textProperty().bindBidirectional(viewModel.getUsername());
+        error.textProperty().bind(viewModel.getError());
         initializeTable();
     }
 
